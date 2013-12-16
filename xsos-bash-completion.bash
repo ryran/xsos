ab1a2d002d39629d0536622f1b45146d8e130b9a
Ryan Sawhill <ryan@b19.org>
2013-12-16 18:28:21 -0500

    forgot xsos-bash-completion

--- xsos-bash-completion.bash
+++ xsos-bash-completion.bash
@@ -1,7 +1,7 @@
 # This file is part of xsos, providing intelligent xsos tab-completion for BASH
 # Save it to: /etc/bash_completion.d/
 #
-# Revision date:  2013/11/10 matching up with xsos v0.2.7
+# Revision date:  2013/11/21 matching up with xsos v0.3.1
 # Latest version: <http://github.com/ryran/xsos>
 # 
 # Copyright 2013 Ryan Sawhill Aroha <rsaw@redhat.com>
@@ -32,12 +32,12 @@ _xsos()  {
   
   # Short and long options
   shrtopts="-h -V -U
-            -a -b -o -c -m -d -t -l -e -g -i -n -s -p
+            -a -b -o -k -c -m -d -t -l -e -g -i -n -s -p
             -q -u -v -w -6
             -x -y -z"
             
   longopts="--help --version --update
-            --all --bios --os --cpu --mem --disks --mpath --lspci --ethtool --bonding --ip --netdev --net --sysctl --ps
+            --all --bios --os --kdump --cpu --mem --disks --mpath --lspci --ethtool --bonding --ip --netdev --net --sysctl --ps
             --B --C --M --D --T --L --G --I --N --P
             --wwid --unit --verbose --width --ipv6
             --nocolor --less --more"
